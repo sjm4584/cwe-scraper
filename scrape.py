2abb31d9bf31eca469e2a3474f03c556f05bea85
sjm4584 <sjm4584@rit.edu>
2015-11-11 15:33:03 -0500

    made things into functions. Now can iterate through a user specified range of cwe's. There's no error checking so it'll probably break.

--- scrape.py
+++ scrape.py
@@ -4,23 +4,71 @@ import bs4
 
 #scrape cwe for cwe name and description
 
-response = requests.get('https://cwe.mitre.org/data/definitions/200.html')
-soup = bs4.BeautifulSoup(response.text, "lxml")
-# Pull 'CWE-##: Name' from the webpage
-headerII = soup.select('h2')
-for header in headerII:
-    print header.string
-    # pull the cwe number so we can get the div with the text
-    cwe_num = header.string.rsplit(':', 1)[0].rsplit('-', 1)[1]
+def make_request(url):
+    return requests.get(url)
 
-# The div with the cwe description is wrapped in a div with the class 
-# "div.oc_200_Description", if the cwe id was 200. We need to pull this div
-# so we can more easily parse for the simple description within the div
-desc_div = soup.find("div", id= "oc_"+cwe_num+"_Description")
+'''
+Name:       create_soup()
+Purpose:    Create our soup object so we can parse things in formulas
+Parameters: <response> which is the web request response we want to parse
+Return:     <soup> our soup object
+'''
+def create_soup(response):
+    soup = bs4.BeautifulSoup(response.text, "lxml")
+    return soup
 
-# now we have to iterate for the div that holds the description we want. Due
-# to the layout of the page we know it's the first description (they didn't use
-# a useful tag
-for tag in desc_div:
-    p = soup.find_all("div", {"class":"indent"})
-    print p[0].text
+
+'''
+Name:       cwe_num_parser()
+Purpose:    Parses for our cwe-number which is needed to parse the description
+Parameters: <response>, the response to our request; <soup> our BeautifulSoup
+            object we are using to parse
+Return:     the cwe number
+'''
+def cwe_num_parser(response, soup):
+    # Pull 'CWE-##: Name' from the webpage
+    headerII = soup.select('h2')
+    for header in headerII:
+        print header.string
+        # pull the cwe number so we can get the div with the text
+        return  header.string.rsplit(':', 1)[0].rsplit('-', 1)[1]
+
+
+'''
+Name:       description_parser()
+Purpose:    Parses the simple description for our cwe
+Parameters: <response>, the response to our request; <cwe_num>, the cwe number
+            which is needed to find the div to parse out; <soup> our
+            BeautifulSoup object we're using to parse
+'''
+def description_parser(response, cwe_num, soup):
+    # The div with the cwe description is wrapped in a div with the class 
+    # "div.oc_200_Description", if the cwe id was 200. We need to pull this div
+    # so we can more easily parse for the simple description within the div
+    desc_div = soup.find("div", id= "oc_"+cwe_num+"_Description")
+
+    # now we have to iterate for the div that holds the description we want. Due
+    # to the layout of the page we know it's the first description (they didn't use
+    # a useful tag
+    for tag in desc_div:
+        p = soup.find_all("div", {"class":"indent"})
+        return p[0].text
+
+
+def main():
+    url = 'https://cwe.mitre.org/data/definitions/1.html'
+    lower_limit = input("[+] Starting record? ")
+    upper_limit = input("[+] Ending record? ")
+
+    for i in range(lower_limit, upper_limit):
+        url = 'https://cwe.mitre.org/data/definitions/'+str(i)+'.html'
+        response = make_request(url)
+        soup = create_soup(response)
+        cwe_num = cwe_num_parser(response, soup)
+        description = description_parser(response, cwe_num, soup)
+        #response = make_request(url)
+        print '[+] ', cwe_num, ':', description
+        print '-'*25
+
+if __name__ == '__main__':
+    main()
